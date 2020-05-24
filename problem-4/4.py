dd0f5610564e0b5fd5afb2c0678f8fd3202d3fb2
PromytheasN <63561890+PromytheasN@users.noreply.github.com>
2020-05-23 21:42:44 -0700

    Update 4.py

--- problem-4/4.py
+++ problem-4/4.py
@@ -2,4 +2,4 @@ def is_palindrome(s):
     return str(s)[::-1] == str(s)
 
 products = [i * j for i in range(100, 999) for j in range(100, 999)]
-print max(filter(is_palindrome, products))
+print (max(filter(is_palindrome, products)))
